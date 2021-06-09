2a10cd2350d6dc459224f7765cc9c3f015d84bd0
Thomas Gormley <thomasgormley415@gmail.com>
2021-06-10 00:14:30 +0100

    adding resolve to config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import reactJsx from 'vite-react-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [reactRefresh(), reactJsx()],
+    resolve: {
+        alias: {
+            __: resolve(__dirname, './src'),
+        },
+    },
 });
