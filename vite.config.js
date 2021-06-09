6f5d1c3a7fe0a526680c3d5bc709a7c182591aaf
Thomas Gormley <thomasgormley415@gmail.com>
2021-06-10 00:36:15 +0100

    node resolve

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import resolve from '@rollup/plugin-node-resolve';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import reactJsx from 'vite-react-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [reactRefresh(), reactJsx()],
-    resolve: {
-        alias: {
-            __: resolve(__dirname, './src'),
+    build: {
+        rollupOptions: {
+            plugins: [resolve()],
         },
     },
 });
